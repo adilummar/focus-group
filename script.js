074892e9fddf2778900349de656018bf88fc0662
adilummar <madilummer@gmail.com>
2024-01-12 17:37:03 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+function redirectToFocusCafe(){
+    window.location.href =  'focuscafe.html';
+}
+
+function redirectToFocusMart(){
+    window.location.href =  'focusmart.html';
+}
+
+function redirectToPopIce(){
+    window.location.href =  'focusmart.html';
+}
